d004c44d97ac43628bda713325530c2b4c07df94
niEmerance <niyemera6@gmail.com>
2019-09-06 16:50:33 +0200

    Create user,Login and create credentials

--- run.py
+++ run.py
@@ -35,3 +35,94 @@ def find_credentials(accountName):
 def display_credentials():
     return Credential.display_credentials()
 
+def main():
+    
+    print("WELCOME TO OUR PASSWORD LOCKER APP")
+    print("***********************************")
+    print("\n")
+
+    while True:
+        # print("Do you have have an account ? (y/n)")
+        # choice=input()
+        # if choice =="n":
+           print("CREATE ACCOUNT")
+           print("**************")
+           print("\n")
+           print("Please Enter your username: ")
+           cUserName=input() 
+           print("Please enter your password")
+           cPassword=input()
+           save_users(create_users(cUserName,cPassword))
+           
+           print("\n")
+           print(f"User {cUserName} created successfully !!")
+           print("\n")
+           
+           print("ENTER YOUR LOGIN CREDENTIALS")
+           print("****************************")
+           print("Enter username: ")
+           lUserName=input()
+           print("Enter Password: ")
+           lPassword=input()
+           
+           if cUserName==lUserName :
+              print("Please Enter :: a:Create a new credential, b:display credentials, c:find a credential, d -exit ")
+              number=input()
+              if number == 'a':
+                 print("NEW CREDENTIAL")
+                 print("Account Name :")
+                 accountName=input()
+                 print("Account username: ")
+                 username=input()
+                 print("Account Password: ")
+                 password=input()
+
+                 save_credentials(create_crededentials(accountName,username,password))
+                 print("\n")
+                 print(f"****Credential {accountName} is created****")
+                 print("\n")
+
+              elif number == 'b':
+                 if display_credentials():
+                    print("Here is all your credentials")
+                    print("\n")
+                                        
+                    for credential in display_credentials():
+                        print(f"Account Name : {accountName}")
+                        print(f"Account username : {username}")
+                        print(f"Account password : {password}")
+                    print("\n")  
+                 else:
+                      print('\n')
+                      print("You dont seem to have any credentials saved yet")
+                      print('\n')
+
+              elif number == 'c' :
+                    print("Enter the account name you want to search for :")
+                    search_name=input()
+                    if  find_credentials(search_name):
+                        search = find_credentials(search_name)
+                        print(f"Account Name : {search.accountName}")
+                        print(f"Account username : {search.username}")
+                        print(f"Account password : {search.password}")
+
+                    else:
+                        print("That credential does not exist")
+
+              elif number == 'd' :
+                    print(f"Thank you {cUserName} for using our app, Bye...")
+                    break
+              else:
+                    print("I really didn't get that. Please use the letters")
+           else:
+                print("Invalid login credentials...")
+        
+        
+
+
+
+
+
+if __name__ == '__main__':
+
+    main()
